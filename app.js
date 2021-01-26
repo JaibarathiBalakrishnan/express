ab53d7774e74f86611fb888127d55c60c9deebab
JAI <mail2jaibarathi@gmail.com>
2021-01-26 21:15:06 +0530

    app.js added root route

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require ('express');
 const bodyParser = require ('body-parser');
 
+//@using of function expresstion in app
 const app = express();
 
+
+//@middle-ware for json objects
 app.use (bodyParser.json());
+
+//creating root route with default middleware function
+app.get('/',(req,res,next) => {
+    res.send('Welcome to Express Server');
+
+})
+
+//@express server listening to 3001
 app.listen(3001);
 console.log('Express Server is running');
